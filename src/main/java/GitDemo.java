ecc6aa3714ccc6c8db4825179a7140d76214936a
kaxn <kaxn33524@gmail.com>
2019-04-23 22:32:30 +0800

    111

---
+++ src/main/java/GitDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: K
+ * @Date: 2019/4/23 22:21
+ * @Version 1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
